017ca9affa0fbc02b77b2a14d76c6cd186b7cff5
David Brodsky <davidpbrodsky@gmail.com>
2015-07-14 13:48:57 -0700

    MessagingFragment: improve btn handling and feedback
    don’t allow multi-presses while operation in progress, show on-screen
    feedback indicating success or error

--- example/src/main/java/org/chatsecure/pushdemo/ui/fragment/MessagingFragment.java
+++ example/src/main/java/org/chatsecure/pushdemo/ui/fragment/MessagingFragment.java
@@ -3,6 +3,7 @@ package org.chatsecure.pushdemo.ui.fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,12 +17,16 @@ import org.chatsecure.pushsecure.pushsecure.PushSecureClient;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import rx.android.schedulers.AndroidSchedulers;
 import timber.log.Timber;
 
 public class MessagingFragment extends Fragment implements View.OnClickListener {
 
     private PushSecureClient client;
     private DataProvider provider;
+
+    @Bind(R.id.container)
+    ViewGroup container;
 
     @Bind(R.id.sharePushTokenButton)
     Button shareTokenButton;
@@ -71,22 +76,44 @@ public class MessagingFragment extends Fragment implements View.OnClickListener 
     }
 
     @Override
-    public void onClick(@NonNull View button) {
+    public void onClick(@NonNull final View button) {
         switch (button.getId()) {
             case R.id.sharePushTokenButton:
+
+                button.setEnabled(false);
                 client.createToken(provider.getGcmToken(), null)
+                        .observeOn(AndroidSchedulers.mainThread())
                         .subscribe(newToken -> {
+                            button.setEnabled(true);
                             Intent shareIntent = new Intent();
                             shareIntent.setAction(Intent.ACTION_SEND);
                             shareIntent.setType("text/plain");
                             shareIntent.putExtra(Intent.EXTRA_TEXT, newToken.token);
                             startActivity(Intent.createChooser(shareIntent, "Share Push Token"));
+                        }, throwable -> {
+                            Timber.e(throwable, "Error fetching new token");
+                            button.setEnabled(true);
+                            Snackbar.make(container, "Error fetching new token", Snackbar.LENGTH_SHORT)
+                                    .show();
                         });
                 break;
 
             case R.id.sendMessageButton:
+
+                button.setEnabled(false);
                 client.sendMessage(peerTokenEditText.getText().toString(), "hello")
-                        .subscribe(message -> Timber.d("Sent message"));
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(message -> {
+                            button.setEnabled(true);
+                            Timber.d("Sent message");
+                            Snackbar.make(container, "Sent message", Snackbar.LENGTH_SHORT)
+                                    .show();
+                        }, throwable -> {
+                            button.setEnabled(true);
+                            Timber.e(throwable, "Error sending message");
+                            Snackbar.make(container, "Error sending message", Snackbar.LENGTH_SHORT)
+                                    .show();
+                        });
                 break;
         }
     }
